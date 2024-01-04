801add289af132ef19b3f1ff1f83e6a16a91c5a9
Javad Fazelpour <117605881+JavadFazelpour@users.noreply.github.com>
2024-01-04 23:20:32 +0330

    Add project files.

---
+++ ShoeStoreApp/ShoeCloset.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeStoreApp
+{
+    internal class ShoeCloset
+    {
+        private readonly List<Shoe> shoes = new List<Shoe>();
+        public void PrintShoes()
+        {
+            if (shoes.Count==0)
+                Console.WriteLine("\nThe shoe closet is empty.");
+            else
+            {
+                Console.WriteLine("\nThe shoe closet contains: ");
+                int i = 1;
+                foreach (Shoe shoe in shoes)
+                {
+                    Console.WriteLine($"Shoe #{i++}: {shoe.Description}");
+                }
+            }
+        }
+        public void AddShoe()
+        {
+
+        }
+        public void RemoveShoe()
+        {
+
+        }
+    }
+}
